0245e0dbf4d81d73d71a995d0beedc29163ce184
Roberto Vetere <Roberto.vetere@gmail.com>
2023-06-19 15:31:59 +0200

    set git comp

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cursoAngular_1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('curso_angular-1 app is running!');
   });
 });
